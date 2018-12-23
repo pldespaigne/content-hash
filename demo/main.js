226f8524c1e201bda8b0dceceaf001db8d00be50
pl.despaigne <pl@despaigne>
2018-12-23 14:38:36 +0100

    adding demo

--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,25 @@
 
 window.onload = () => {
+	console.log('contentHash')
 
-	
+	const ipfsInputElem = document.getElementById('ipfs-input')
+	const ipfsButtonElem = document.getElementById('ipfs-encode')
+	const ipfsResultElem = document.getElementById('ipfs-result')
+	ipfsButtonElem.addEventListener('click', () => {
+		ipfsResultElem.innerHTML = contentHash.fromIpfs(ipfsInputElem.value).toString('hex')
+	})
 
+	const swarmInputElem = document.getElementById('swarm-input')
+	const swarmButtonElem = document.getElementById('swarm-encode')
+	const swarmResultElem = document.getElementById('swarm-result')
+	swarmButtonElem.addEventListener('click', () => {
+		swarmResultElem.innerHTML = contentHash.fromSwarm(swarmInputElem.value).toString('hex')
+	})
+
+	const contentInputElem = document.getElementById('content-input')
+	const contentButtonElem = document.getElementById('content-decode')
+	const contentResultElem = document.getElementById('content-result')
+	contentButtonElem.addEventListener('click', () => {
+		contentResultElem.innerHTML = contentHash.decode(contentHash.hexStringToBuffer(contentInputElem.value))
+	})
 }
