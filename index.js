c706db9b603dbacd126b6d91f2ba725201c904af
ricott1 <ricott2@gmail.com>
2019-05-31 20:41:36 -0400

    onion support

--- index.js
+++ index.js
@@ -27,76 +27,97 @@ const packageJson = require('./package.json')
  * @return {Buffer} the resulting Buffer
  */
 function hexString(hex) {
-	let prefix = hex.slice(0, 2)
-	let value = hex.slice(2)
-	let res = ''
-	if (prefix === '0x') res = value
-	else res = hex
-	return multiH.fromHexString(res)
+    let prefix = hex.slice(0, 2)
+    let value = hex.slice(2)
+    let res = ''
+    if (prefix === '0x') res = value
+    else res = hex
+    return multiH.fromHexString(res)
 }
 
 module.exports = {
-	version: packageJson.version,
+    version: packageJson.version,
 
-	/**
-	* Decode a Content Hash.
-	* @param {string} hash an hex string containing a content hash
-	* @return {string} the decoded content
-	*/
-	decode: function (hash) {
+    /**
+     * Decode a Content Hash.
+     * @param {string} hash an hex string containing a content hash
+     * @return {string} the decoded content
+     */
+    decode: function(hash) {
 
-		let buffer = hexString(hash)
-		
-		const codec = multiC.getCodec(buffer)	// get the codec
-		let value = multiC.rmPrefix(buffer)		// get the remaining value
-		let cid = new CID(value)				// prepare a cid from value in case the codec is of type ipfs or swarm
+        let buffer = hexString(hash)
 
-		let res = value.toString('hex')
+        const codec = multiC.getCodec(buffer) // get the codec
+        let value = multiC.rmPrefix(buffer) // get the remaining value
+        let cid = new CID(value) // prepare a cid from value in case the codec is of type ipfs or swarm
 
-		if (codec === 'swarm-ns'){
-			value = cid.multihash
-			res = multiH.decode(value).digest.toString('hex')
-		} else if (codec === 'ipfs-ns'){
-			value = cid.multihash	
-			res = multiH.toB58String(value)
-		} else { // if codec is not of type ipfs/swarm just return the remaining value
-			console.warn('⚠️ WARNING ⚠️ : unknown codec ' + codec.toString('hex') + ' for content-hash ' + res)
-		}
-		return res
-	},
+        let res = value.toString('hex')
 
-	/**
-	* Encode an IPFS address into a content hash
-	* @param {string} ipfsHash string containing an IPFS address
-	* @return {string} the resulting content hash
-	*/
-	fromIpfs: function (ipfsHash) {
-		let multihash = multiH.fromB58String(ipfsHash)	// get Multihash buffer
-		let res = new CID(1, 'dag-pb', multihash)		// create a CIDv1 with the multihash
-		res = multiC.addPrefix('ipfs-ns', res.buffer)	// add ipfs codec prefix
-		return res.toString('hex')
-	},
+        if (codec === 'swarm-ns') {
+            value = cid.multihash
+            res = multiH.decode(value).digest.toString('hex')
+        } else if (codec === 'ipfs-ns') {
+            value = cid.multihash
+            res = multiH.toB58String(value)
+        } else if (codec === 'onion' || codec === 'onion') { 
+            res = multiH.decode(buffer).digest.toString()
+        } else { // if codec is not of type ipfs/swarm/onion just return the remaining value
+            console.warn('⚠️ WARNING ⚠️ : unknown codec ' + codec.toString('hex') + ' for content-hash ' + res)
+        }
+        return res
+    },
 
-	/**
-	* Encode a Swarm address into a content hash
-	* @param {string} swarmHash string containing a Swarm address
-	* @return {string} the resulting content hash
-	*/
-	fromSwarm: function (swarmHash) {
-		swarmHash = hexString(swarmHash)
-		let multihash = multiH.encode(swarmHash, 'keccak-256')	// get Multihash buffer
-		let res = new CID(1, 'swarm-manifest', multihash)				// create a CIDv1 with the multihash
-		res = multiC.addPrefix('swarm-ns', res.buffer)			// add swarm codec prefix
-		return res.toString('hex')
-	},
+    /**
+     * Encode an IPFS address into a content hash
+     * @param {string} ipfsHash string containing an IPFS address
+     * @return {string} the resulting content hash
+     */
+    fromIpfs: function(ipfsHash) {
+        let multihash = multiH.fromB58String(ipfsHash) // get Multihash buffer
+        let res = new CID(1, 'dag-pb', multihash) // create a CIDv1 with the multihash
+        res = multiC.addPrefix('ipfs-ns', res.buffer) // add ipfs codec prefix
+        return res.toString('hex')
+    },
 
-	/**
-	* Extract the codec of a content hash
-	* @param {string} hash hex string containing a content hash
-	* @return {string} the extracted codec
-	*/
-	getCodec: function (hash) {
-		let buffer = hexString(hash)
-		return multiC.getCodec(buffer)
-	},
+    /**
+     * Encode a Swarm address into a content hash
+     * @param {string} swarmHash string containing a Swarm address
+     * @return {string} the resulting content hash
+     */
+    fromSwarm: function(swarmHash) {
+        swarmHash = hexString(swarmHash)
+        let multihash = multiH.encode(swarmHash, 'keccak-256') // get Multihash buffer
+        let res = new CID(1, 'swarm-manifest', multihash) // create a CIDv1 with the multihash
+        res = multiC.addPrefix('swarm-ns', res.buffer) // add swarm codec prefix
+        return res.toString('hex')
+    },
+
+    /**
+     * Encode an Onion address into a content hash
+     * @param {string} onionHash string containing an Onion address
+     * @return {string} the resulting content hash
+     */
+    fromOnion: function(onionHash) {
+        let code
+        let buffer = Buffer.from(onionHash)
+        if (onionHash.length == 56) {
+            code = 'onion3'
+        } else if (onionHash.length == 16) {
+            code = 'onion'
+        } else {
+            console.warn("Invalid onion address")
+        }
+        let res = multiH.encode(buffer, code)
+        return res.toString('hex')
+    },
+
+    /**
+     * Extract the codec of a content hash
+     * @param {string} hash hex string containing a content hash
+     * @return {string} the extracted codec
+     */
+    getCodec: function(hash) {
+        let buffer = hexString(hash)
+        return multiC.getCodec(buffer)
+    },
 }
