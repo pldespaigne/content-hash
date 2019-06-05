3ae5144871fefb4402d5d738aea8bf9a5789968a
ricott1 <ricott2@gmail.com>
2019-06-04 22:44:27 -0400

    encoding with multiaddr

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 /*
-	ISC License
+    ISC License
 
-	Copyright (c) 2019, Pierre-Louis Despaigne
+    Copyright (c) 2019, Pierre-Louis Despaigne
 
-	Permission to use, copy, modify, and/or distribute this software for any
-	purpose with or without fee is hereby granted, provided that the above
-	copyright notice and this permission notice appear in all copies.
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
 
-	THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-	WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-	MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-	ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-	WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-	ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-	OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
 const CID = require('cids')
 const multiC = require('multicodec')
 const multiH = require('multihashes')
+const multiA = require('multiaddr')
 const packageJson = require('./package.json')
 
 /**
@@ -49,13 +50,13 @@ module.exports = {
 
         const codec = multiC.getCodec(buffer) // get the codec
         let value = multiC.rmPrefix(buffer) // get the remaining value
-        // prepare a cid from value in case the codec is of type ipfs or swarm
-
         let res = value.toString('hex')
         if (codec === 'onion' || codec === 'onion3') {
-            return multiH.decode(buffer).digest.toString()
+            const multiAddr = multiA(buffer)
+            return multiAddr.toString()
         }
         if (codec === 'swarm-ns' || codec === 'ipfs-ns') {
+            // prepare a cid from value in case the codec is of type ipfs or swarm
             let cid = new CID(value)
             value = cid.multihash
             if (codec === 'swarm-ns')
@@ -98,18 +99,23 @@ module.exports = {
      * @param {string} onionHash string containing an Onion address
      * @return {string} the resulting content hash
      */
-    fromOnion: function(onionHash) {
-        let code
-        let buffer = Buffer.from(onionHash)
-        if (onionHash.length == 56) {
-            code = 'onion3'
-        } else if (onionHash.length == 16) {
-            code = 'onion'
-        } else {
-            console.warn("Invalid onion address")
+    fromOnion: function(onionAddr) {
+        let addr, port, code
+        let content = onionAddr.split(':')
+        if (content.length === 2) {
+            port = content[1]
         }
-        let res = multiH.encode(buffer, code)
-        return res.toString('hex')
+        addr = content[0]
+        if (addr.length === 16) {
+            code = '/onion/'
+        } else if (addr.length === 56) {
+            code = '/onion3/'
+        } else {
+            throw new Error('Invalid onion address')
+        }
+        const compositeAddr = code + addr + ':' + port
+        res = multiA(compositeAddr).buffer.toString('hex')
+        return res
     },
 
     /**
