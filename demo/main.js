b77c96fb6e1b61f7add40b31833eaaf4368cb509
Filip Š <filip.stamcar@hotmail.com>
2019-07-29 13:04:59 +0200

    Update demo

--- demo/main.js
+++ demo/main.js
@@ -27,10 +27,16 @@ window.onload = () => {
 		
 		if(contentHash.getCodec(contentInputElem.value) === 'ipfs-ns')codec = 'ipfs'
 		else if(contentHash.getCodec(contentInputElem.value) === 'swarm-ns')codec = 'swarm'
+		else if(contentHash.getCodec(contentInputElem.value) === 'onion')codec = 'onion'
+		else if(contentHash.getCodec(contentInputElem.value) === 'onion3')codec = 'onion3'
+		else if(contentHash.getCodec(contentInputElem.value) === 'zeronet')codec = 'zeronet'
 
 		let url = 'https://'
 		if(codec === 'ipfs') url += 'gateway.ipfs.io/ipfs/' + cth + '/'
 		else if(codec === 'swarm') url += 'swarm-gateways.net/bzz:/' + cth + '/'
+		else if(codec === 'onion') url += cth + '/'
+		else if(codec === 'onion3') url += cth + '/'
+		else if(codec === 'zeronet') url = 'http://127.0.0.1:43110/' + cth + '/'
 		else url = '#'
 
 		codecResultElem.innerHTML = 'codec : ' + codec
